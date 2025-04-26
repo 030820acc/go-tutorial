655e81bbeb56f4bedd2d78bfe1bbc2bf45a97615
030820acc <030820acc@gmail.com>
2025-04-26 00:27:24 -0400

    added swtich statement practice and mod path practice

---
+++ switchStatements/go.mod
@@ -0,0 +1,3 @@
+module 030820acc/go-tutorial.git/switchStatements
+
+go 1.24.2
---
+++ switchStatements/switchPractice.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Enter your favorite Adventure Time character: ")
+	scanner.Scan()
+	var answer = scanner.Text()
+	switch {
+	case answer == "ice king":
+		fmt.Println("simple simon")
+	case answer == "finn":
+		fmt.Println("he has a hero heart")
+	case answer == "jake":
+		fmt.Println("sucking at making CLI tools is the first step ")
+		fmt.Println("towards being sort of good at making CLI tools")
+	case answer == "lsp":
+		fmt.Println("oh my glob")
+	default:
+		fmt.Println("thats a good choice")
+	}
+}
